a6b68dce0ae2e2f59fe789935d0137e3ff6d1f6c
Wolfgang Merkt <w.merkt@gmail.com>
2024-06-07 15:39:08 +0100

    Rebase from 'release/humble/proxsuite'

--- bindings/python/src/algorithms.hpp
+++ bindings/python/src/algorithms.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2022 INRIA
+// Copyright (c) 2022-2023 INRIA
 //
 #ifndef proxsuite_python_algorithms_hpp
 #define proxsuite_python_algorithms_hpp
@@ -9,6 +9,11 @@
 #include "expose-settings.hpp"
 #include "expose-workspace.hpp"
 #include "expose-qpobject.hpp"
+#include "expose-qpvector.hpp"
 #include "expose-solve.hpp"
-
+#include "expose-helpers.hpp"
+#include "expose-backward.hpp"
+#ifdef PROXSUITE_PYTHON_INTERFACE_WITH_OPENMP
+#include "expose-parallel.hpp"
+#endif
 #endif /* end of include guard proxsuite_python_algorithms_hpp */
