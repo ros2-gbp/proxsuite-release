c7833595848debf45acffab4e14204a302f9215d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-27 10:26:15 +0200

    Rebase from 'debian/kilted/proxsuite'

--- cmake-module/release.cmake
+++ cmake-module/release.cmake
@@ -58,63 +58,76 @@ macro(RELEASE_SETUP)
       set(LD_LIBRARY_PATH_VARIABLE_NAME "LD_LIBRARY_PATH")
     endif(APPLE)
 
+    if(NOT TARGET release_package_xml)
+      add_custom_target(release_package_xml COMMENT "Update package.xml")
+    endif()
     add_custom_target(
-      release_package_xml
+      ${PROJECT_NAME}-release_package_xml
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Update package.xml"
+      COMMENT "Update package.xml for ${PROJECT_NAME}"
       COMMAND
         sed -i.back \"s|<version>.*</version>|<version>$$VERSION</version>|g\"
         package.xml && rm package.xml.back && ${GIT} add package.xml && ${GIT}
         commit -m "release: Update package.xml version to $$VERSION" && echo
-        "Updated package.xml and committed")
+        "Updated package.xml and committed"
+    )
+    add_dependencies(release_package_xml ${PROJECT_NAME}-release_package_xml)
 
+    if(NOT TARGET release_pyproject_toml)
+      add_custom_target(release_pyproject_toml COMMENT "Update pyproject.toml")
+    endif()
     add_custom_target(
-      release_pyproject_toml
+      ${PROJECT_NAME}-release_pyproject_toml
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Update pyproject.toml"
+      COMMENT "Update pyproject.toml for ${PROJECT_NAME}"
       COMMAND
         ${PYTHON_EXECUTABLE} ${PROJECT_JRL_CMAKE_MODULE_DIR}/pyproject.py
         $$VERSION && if ! (git diff --quiet pyproject.toml) ; then
-        (${GIT}
-         add
-         pyproject.toml
-         &&
-         ${GIT}
-         commit
-         -m
-         "release: Update pyproject.toml version to $$VERSION"
-         &&
-         echo
-         "Updated pyproject.toml and committed") ; fi)
+        (
+          ${GIT}
+          add pyproject.toml && ${GIT} commit -m
+          "release: Update pyproject.toml version to $$VERSION" && echo
+          "Updated pyproject.toml and committed"
+        )
+        ; fi
+    )
+    add_dependencies(
+      release_pyproject_toml
+      ${PROJECT_NAME}-release_pyproject_toml
+    )
 
+    if(NOT TARGET release_changelog)
+      add_custom_target(release_changelog COMMENT "Update CHANGELOG.md")
+    endif()
     add_custom_target(
-      release_changelog
+      ${PROJECT_NAME}-release_changelog
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Update CHANGELOG.md"
+      COMMENT "Update CHANGELOG.md for ${PROJECT_NAME}"
       COMMAND
         sed -i.back
         "\"s|\#\# \\[Unreleased\\]|\#\# [Unreleased]\\n\\n\#\# [$$VERSION] - ${TODAY}|\""
         CHANGELOG.md && sed -i.back
         "\"s|^\\[Unreleased]: \\(https://.*compare/\\)\\(v.*\\)...HEAD|[Unreleased]: \\1v$$VERSION...HEAD\\n[$$VERSION]: \\1\\2...v$$VERSION|\""
         CHANGELOG.md && if ! (git diff --quiet CHANGELOG.md) ; then
-        (${GIT}
-         add
-         CHANGELOG.md
-         &&
-         ${GIT}
-         commit
-         -m
-         "release: Update CHANGELOG.md for $$VERSION"
-         &&
-         echo
-         "Updated CHANGELOG.md and committed") ; fi)
+        (
+          ${GIT}
+          add CHANGELOG.md && ${GIT} commit -m
+          "release: Update CHANGELOG.md for $$VERSION" && echo
+          "Updated CHANGELOG.md and committed"
+        )
+        ; fi
+    )
+    add_dependencies(release_changelog ${PROJECT_NAME}-release_changelog)
 
+    if(NOT TARGET release_pixi_toml)
+      add_custom_target(release_pixi_toml COMMENT "Update pixi.toml")
+    endif()
+    # gersemi: off
     add_custom_target(
-      release_pixi_toml
+      ${PROJECT_NAME}-release_pixi_toml
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Update pixi.toml"
+      COMMENT "Update pixi.toml for ${PROJECT_NAME}"
       COMMAND
-        # cmake-format: off
         ${PYTHON_EXECUTABLE} ${PROJECT_JRL_CMAKE_MODULE_DIR}/pixi.py $$VERSION &&
         if ! (git diff --quiet pixi.toml) ; then
         (
@@ -122,57 +135,67 @@ macro(RELEASE_SETUP)
          ${GIT} commit -m "release: Update pixi.toml version to $$VERSION" &&
          echo "Updated pixi.toml and committed"
         ) ; fi
-# cmake-format: on
     )
+    # gersemi: on
+    add_dependencies(release_pixi_toml ${PROJECT_NAME}-release_pixi_toml)
 
+    if(NOT TARGET release_citation_cff)
+      add_custom_target(release_citation_cff COMMENT "Update CITATION.cff")
+    endif()
     add_custom_target(
-      release_citation_cff
+      ${PROJECT_NAME}-release_citation_cff
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Update CITATION.cff"
+      COMMENT "Update CITATION.cff for ${PROJECT_NAME}"
       COMMAND
         sed -i.back
         "\"s|^version:.*|version: $$VERSION|;s|^date-released:.*|date-released: \\\"${TODAY}\\\"|\""
         CITATION.cff && rm CITATION.cff.back && ${GIT} add CITATION.cff &&
         ${GIT} commit -m "release: Update CITATION.cff version to $$VERSION" &&
-        echo "Updated CITATION.cff and committed")
+        echo "Updated CITATION.cff and committed"
+    )
+    add_dependencies(release_citation_cff ${PROJECT_NAME}-release_citation_cff)
 
-    set(BUILD_CMD ${CMAKE_COMMAND} --build ${CMAKE_BINARY_DIR} --target)
+    set(BUILD_CMD ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR} --target)
+    if(NOT TARGET release)
+      add_custom_target(release COMMENT "Create a new release")
+    endif()
+    # gersemi: off
     add_custom_target(
-      release
+      ${PROJECT_NAME}-release
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      COMMENT "Create a new release"
+      COMMENT "Create a new release for ${PROJECT_NAME}"
       COMMAND
-        # cmake-format: off
         export LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH} &&
         export ${LD_LIBRARY_PATH_VARIABLE_NAME}=$ENV{${LD_LIBRARY_PATH_VARIABLE_NAME}} &&
         export PYTHONPATH=$ENV{PYTHONPATH} &&
         ! test x$$VERSION = x || (echo "Please set a version for this release" && false) &&
         # Update version in package.xml if it exists
-        if [ -f "package.xml" ]; then (${BUILD_CMD} release_package_xml) ; fi &&
+        if [ -f "package.xml" ]; then (${BUILD_CMD} ${PROJECT_NAME}-release_package_xml) ; fi &&
         # Update version in pyproject.toml if it exists
-        if [ -f "pyproject.toml" ]; then (${BUILD_CMD} release_pyproject_toml) ; fi &&
+        if [ -f "pyproject.toml" ]; then (${BUILD_CMD} ${PROJECT_NAME}-release_pyproject_toml) ; fi &&
         # Update CHANGELOG.md if it exists
-        if [ -f "CHANGELOG.md" ]; then (${BUILD_CMD} release_changelog) ; fi &&
+        if [ -f "CHANGELOG.md" ]; then (${BUILD_CMD} ${PROJECT_NAME}-release_changelog) ; fi &&
         # Update version in pixi.toml if it exists
-        if [ -f "pixi.toml" ]; then (${BUILD_CMD} release_pixi_toml) ; fi &&
+        if [ -f "pixi.toml" ]; then (${BUILD_CMD} ${PROJECT_NAME}-release_pixi_toml) ; fi &&
         # Update date and version in CITATION.cff if it exists
-        if [ -f "CITATION.cff" ]; then (${BUILD_CMD} release_citation_cff) ; fi &&
+        if [ -f "CITATION.cff" ]; then (${BUILD_CMD} ${PROJECT_NAME}-release_citation_cff) ; fi &&
         ${GIT} tag -s v$$VERSION -m "Release of version $$VERSION." &&
-        cd ${CMAKE_BINARY_DIR} &&
+        cd ${PROJECT_BINARY_DIR} &&
         cmake ${PROJECT_SOURCE_DIR} &&
-        ${BUILD_CMD} distcheck ||
+        ${BUILD_CMD} ${PROJECT_NAME}-distcheck ||
         (
          echo "Please fix distcheck first." &&
          cd ${PROJECT_SOURCE_DIR} &&
          ${GIT} tag -d v$$VERSION &&
-         cd ${CMAKE_BINARY_DIR} &&
+         cd ${PROJECT_BINARY_DIR} &&
          cmake ${PROJECT_SOURCE_DIR} &&
          false
         ) &&
-        ${BUILD_CMD} dist &&
-        ${BUILD_CMD} distclean &&
+        ${BUILD_CMD} ${PROJECT_NAME}-dist &&
+        ${BUILD_CMD} ${PROJECT_NAME}-distclean &&
         echo "Please, run 'git push --tags' and upload the tarball to github to finalize this release."
-# cmake-format: on
     )
+    # gersemi: on
+    add_dependencies(release ${PROJECT_NAME}-release)
   endif()
 endmacro()
