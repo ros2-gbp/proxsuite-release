fa6c2009a418909849e4d8fba9add7f02d0b7f5e
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-27 10:20:27 +0200

    Rebase from 'upstream'

--- cmake-module/test.cmake
+++ cmake-module/test.cmake
@@ -32,15 +32,13 @@
 # *test*. unit-test  is added and all tests added with
 if(DEFINED DISABLE_TESTS)
   message(
-    AUTHOR_WARNING "DISABLE_TESTS is deprecated. Use BUILD_TESTING instead.")
+    AUTHOR_WARNING
+    "DISABLE_TESTS is deprecated. Use BUILD_TESTING instead."
+  )
   if(DISABLE_TESTS)
-    set(BUILD_TESTING
-        OFF
-        CACHE BOOL "")
+    set(BUILD_TESTING OFF CACHE BOOL "")
   else()
-    set(BUILD_TESTING
-        ON
-        CACHE BOOL "")
+    set(BUILD_TESTING ON CACHE BOOL "")
   endif()
 endif(DEFINED DISABLE_TESTS)
 
@@ -50,10 +48,15 @@ endif()
 
 # Add new target 'run_tests' to improve integration with build tooling
 if(NOT CMAKE_GENERATOR MATCHES "Visual Studio|Xcode" AND NOT TARGET run_tests)
+  if(NOT TARGET run_tests)
+    add_custom_target(run_tests)
+  endif()
   add_custom_target(
-    run_tests
+    ${PROJECT_NAME}-run_tests
     COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure -V
-    VERBATIM)
+    VERBATIM
+  )
+  add_dependencies(run_tests ${PROJECT_NAME}-run_tests)
 endif()
 
 if(NOT DEFINED ctest_build_tests_exists)
@@ -65,19 +68,23 @@ endif(NOT DEFINED ctest_build_tests_exists)
 # Create target ctest_build_tests if does not exist yet.
 #
 macro(CREATE_CTEST_BUILD_TESTS_TARGET)
-  get_property(ctest_build_tests_exists_value GLOBAL
-               PROPERTY ctest_build_tests_exists)
+  get_property(
+    ctest_build_tests_exists_value
+    GLOBAL
+    PROPERTY ctest_build_tests_exists
+  )
   if(NOT BUILD_TESTING)
     if(NOT ctest_build_tests_exists_value)
       add_test(
         ctest_build_tests
         "${CMAKE_COMMAND}"
         --build
-        ${CMAKE_BINARY_DIR}
+        ${PROJECT_BINARY_DIR}
         --target
         build_tests
         --
-        $ENV{MAKEFLAGS})
+        $ENV{MAKEFLAGS}
+      )
       set_property(GLOBAL PROPERTY ctest_build_tests_exists ON)
     endif(NOT ctest_build_tests_exists_value)
   endif(NOT BUILD_TESTING)
@@ -88,7 +95,7 @@ endmacro(CREATE_CTEST_BUILD_TESTS_TARGET)
 # The behaviour of this function depends on :variable:`BUILD_TESTING` option.
 #
 macro(ADD_UNIT_TEST NAME)
-  create_ctest_build_tests_target()
+  CREATE_CTEST_BUILD_TESTS_TARGET()
 
   if(NOT BUILD_TESTING)
     add_executable(${NAME} EXCLUDE_FROM_ALL ${ARGN})
@@ -98,30 +105,37 @@ macro(ADD_UNIT_TEST NAME)
 
   add_dependencies(build_tests ${NAME})
 
-  add_test(NAME ${NAME} COMMAND ${NAME})
+  if(ENABLE_COVERAGE)
+    add_test(
+      NAME ${NAME}
+      COMMAND
+        ${KCOV} --include-path=${CMAKE_SOURCE_DIR} ${KCOV_DIR}/${NAME} ${NAME}
+    )
+  else()
+    add_test(NAME ${NAME} COMMAND ${NAME})
+  endif()
   # Support definition of DYLD_LIBRARY_PATH for OSX systems
   if(APPLE)
     set_tests_properties(
       ${NAME}
       PROPERTIES
         ENVIRONMENT
-        "LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH};DYLD_LIBRARY_PATH=$ENV{DYLD_LIBRARY_PATH}"
+          "LD_LIBRARY_PATH=$ENV{LD_LIBRARY_PATH};DYLD_LIBRARY_PATH=$ENV{DYLD_LIBRARY_PATH}"
     )
   endif(APPLE)
 
   if(NOT BUILD_TESTING)
     set_tests_properties(${NAME} PROPERTIES DEPENDS ctest_build_tests)
   endif(NOT BUILD_TESTING)
-endmacro(
-  ADD_UNIT_TEST
-  NAME
-  SOURCE)
+endmacro(ADD_UNIT_TEST NAME SOURCE)
 
 # .rst: .. command:: COMPUTE_PYTHONPATH (result [MODULES...])
 #
 # Fill `result` with all necessary environment variables (`PYTHONPATH`,
 # `LD_LIBRARY_PATH`, `DYLD_LIBRARY_PATH`) to load the `MODULES` in
-# `CMAKE_BINARY_DIR` (`CMAKE_BINARY_DIR/MODULE_PATH`)
+# `PROJECT_BINARY_DIR` (`PROJECT_BINARY_DIR/MODULE_PATH`)
+#
+# Path in PROJECT_PYTHON_PACKAGES_IN_WORKSPACE are added to the PYTHONPATH.
 #
 # .. note:: :command:`FINDPYTHON` should have been called first.
 #
@@ -129,9 +143,9 @@ function(COMPUTE_PYTHONPATH result)
   set(MODULES "${ARGN}") # ARGN is not a variable
   foreach(MODULE_PATH IN LISTS MODULES)
     if(CMAKE_GENERATOR MATCHES "Visual Studio|Xcode")
-      list(APPEND PYTHONPATH "${CMAKE_BINARY_DIR}/${MODULE_PATH}/$<CONFIG>")
+      list(APPEND PYTHONPATH "${PROJECT_BINARY_DIR}/${MODULE_PATH}/$<CONFIG>")
     else()
-      list(APPEND PYTHONPATH "${CMAKE_BINARY_DIR}/${MODULE_PATH}")
+      list(APPEND PYTHONPATH "${PROJECT_BINARY_DIR}/${MODULE_PATH}")
     endif()
   endforeach(MODULE_PATH IN LISTS MODULES)
 
@@ -139,11 +153,14 @@ function(COMPUTE_PYTHONPATH result)
     list(APPEND PYTHONPATH "$ENV{PYTHONPATH}")
   endif(DEFINED ENV{PYTHONPATH})
 
+  list(APPEND PYTHONPATH ${PROJECT_PYTHON_PACKAGES_IN_WORKSPACE})
+
   # get path separator to join those paths
   execute_process(
     COMMAND "${PYTHON_EXECUTABLE}" "-c" "import os; print(os.pathsep)"
     OUTPUT_VARIABLE PATHSEP
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
 
   list(REMOVE_DUPLICATES PYTHONPATH)
   if(WIN32)
@@ -159,55 +176,71 @@ function(COMPUTE_PYTHONPATH result)
     list(APPEND ENV_VARIABLES "DYLD_LIBRARY_PATH=$ENV{DYLD_LIBRARY_PATH}")
   endif(APPLE)
 
-  set(${result}
-      ${ENV_VARIABLES}
-      PARENT_SCOPE)
+  set(${result} ${ENV_VARIABLES} PARENT_SCOPE)
 endfunction()
 
 # .rst: .. command:: ADD_PYTHON_UNIT_TEST (NAME SOURCE [MODULES...])
 #
 # Add a test called `NAME` that runs an equivalent of ``python ${SOURCE}``,
-# optionnaly with a `PYTHONPATH` set to `CMAKE_BINARY_DIR/MODULE_PATH` for each
-# MODULES `SOURCE` is relative to `PROJECT_SOURCE_DIR`
+# optionnaly with a `PYTHONPATH` set to `PROJECT_BINARY_DIR/MODULE_PATH` for
+# each MODULES `SOURCE` is relative to `PROJECT_SOURCE_DIR`
 #
 # .. note:: :command:`FINDPYTHON` should have been called first.
 #
 macro(ADD_PYTHON_UNIT_TEST NAME SOURCE)
   if(ENABLE_COVERAGE)
-    set_property(GLOBAL PROPERTY JRL_CMAKEMODULES_HAS_PYTHON_COVERAGE ON)
-    set(PYTHONPATH "${CMAKE_INSTALL_PREFIX}/${PYTHON_SITELIB}")
+    # run this python test to gather C++ coverage of python bindings
     add_test(
       NAME ${NAME}
-      COMMAND ${PYTHON_EXECUTABLE} -m coverage run --branch -p
-              --source=${PYTHONPATH} "${PROJECT_SOURCE_DIR}/${SOURCE}"
-      WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+      COMMAND
+        ${KCOV} --include-path=${CMAKE_SOURCE_DIR} ${KCOV_DIR}/${NAME}
+        ${PYTHON_EXECUTABLE} "${PROJECT_SOURCE_DIR}/${SOURCE}"
+    )
+    # run this python test again, but this time to gather python coverage
+    add_test(
+      NAME ${NAME}-pycov
+      COMMAND
+        ${KCOV} --include-path=${CMAKE_SOURCE_DIR} ${KCOV_DIR}/${NAME}
+        "${PROJECT_SOURCE_DIR}/${SOURCE}"
+    )
   else()
-    add_test(NAME ${NAME} COMMAND ${PYTHON_EXECUTABLE}
-                                  "${PROJECT_SOURCE_DIR}/${SOURCE}")
-    set(PYTHONPATH)
+    add_test(
+      NAME ${NAME}
+      COMMAND ${PYTHON_EXECUTABLE} "${PROJECT_SOURCE_DIR}/${SOURCE}"
+    )
   endif()
 
   set(MODULES "${ARGN}") # ARGN is not a variable
-  compute_pythonpath(ENV_VARIABLES ${MODULES})
+  set(PYTHONPATH)
+  COMPUTE_PYTHONPATH(ENV_VARIABLES ${MODULES})
   set_tests_properties(${NAME} PROPERTIES ENVIRONMENT "${ENV_VARIABLES}")
-endmacro(
-  ADD_PYTHON_UNIT_TEST
-  NAME
-  SOURCE)
+  if(ENABLE_COVERAGE)
+    set_tests_properties(
+      ${NAME}-pycov
+      PROPERTIES ENVIRONMENT "${ENV_VARIABLES}"
+    )
+  endif()
+endmacro(ADD_PYTHON_UNIT_TEST NAME SOURCE)
 
 # .rst: .. command:: ADD_PYTHON_MEMORYCHECK_UNIT_TEST (NAME SOURCE [MODULES...])
 #
 # Add a test called `NAME` that runs an equivalent of ``valgrind -- python
 # ${SOURCE}``, optionnaly with a `PYTHONPATH` set to
-# `CMAKE_BINARY_DIR/MODULE_PATH` for each MODULES. `SOURCE` is relative to
+# `PROJECT_BINARY_DIR/MODULE_PATH` for each MODULES. `SOURCE` is relative to
 # `PROJECT_SOURCE_DIR`.
 #
 # .. note:: :command:`FINDPYTHON` should have been called first. .. note:: Only
 # work if valgrind is installed
 #
 macro(ADD_PYTHON_MEMORYCHECK_UNIT_TEST NAME SOURCE)
-  add_python_memorycheck_unit_test_v2(NAME ${NAME} SOURCE ${SOURCE} MODULES
-                                      ${ARGN})
+  ADD_PYTHON_MEMORYCHECK_UNIT_TEST_V2(
+    NAME
+    ${NAME}
+    SOURCE
+    ${SOURCE}
+    MODULES
+    ${ARGN}
+  )
 endmacro()
 
 # ~~~
@@ -229,7 +262,7 @@ endmacro()
 # source dir.
 #
 # :param MODULES: Set the `PYTHONPATH` environment variable to
-# `CMAKE_BINARY_DIR/<modules>...`.
+# `PROJECT_BINARY_DIR/<modules>...`.
 #
 # .. note:: :command:`FINDPYTHON` should have been called first.
 #
@@ -239,8 +272,13 @@ macro(ADD_PYTHON_MEMORYCHECK_UNIT_TEST_V2)
     set(options)
     set(oneValueArgs NAME SOURCE SUPP)
     set(multiValueArgs MODULES)
-    cmake_parse_arguments(ARGS "${options}" "${oneValueArgs}"
-                          "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(
+      ARGS
+      "${options}"
+      "${oneValueArgs}"
+      "${multiValueArgs}"
+      ${ARGN}
+    )
 
     set(TEST_FILE_NAME memorycheck_unit_test_${ARGS_NAME}.cmake)
     set(PYTHON_TEST_SCRIPT "${PROJECT_SOURCE_DIR}/${ARGS_SOURCE}")
@@ -249,11 +287,13 @@ macro(ADD_PYTHON_MEMORYCHECK_UNIT_TEST_V2)
     endif()
     configure_file(
       ${PROJECT_JRL_CMAKE_MODULE_DIR}/memorycheck_unit_test.cmake.in
-      ${TEST_FILE_NAME} @ONLY)
+      ${TEST_FILE_NAME}
+      @ONLY
+    )
 
     add_test(NAME ${ARGS_NAME} COMMAND ${CMAKE_COMMAND} -P ${TEST_FILE_NAME})
 
-    compute_pythonpath(ENV_VARIABLES ${ARGS_MODULES})
+    COMPUTE_PYTHONPATH(ENV_VARIABLES ${ARGS_MODULES})
     set_tests_properties(${ARGS_NAME} PROPERTIES ENVIRONMENT "${ENV_VARIABLES}")
   endif()
 endmacro()
@@ -263,12 +303,11 @@ endmacro()
 # Add a test called `NAME` that runs an equivalent of ``julia ${SOURCE}``.
 #
 macro(ADD_JULIA_UNIT_TEST NAME SOURCE)
-  add_test(NAME ${NAME} COMMAND ${Julia_EXECUTABLE}
-                                "${PROJECT_SOURCE_DIR}/${SOURCE}")
-endmacro(
-  ADD_JULIA_UNIT_TEST
-  NAME
-  SOURCE)
+  add_test(
+    NAME ${NAME}
+    COMMAND ${Julia_EXECUTABLE} "${PROJECT_SOURCE_DIR}/${SOURCE}"
+  )
+endmacro(ADD_JULIA_UNIT_TEST NAME SOURCE)
 
 # DEFINE_UNIT_TEST(NAME LIB)
 # ----------------------
@@ -276,6 +315,6 @@ endmacro(
 # Compile a program and add it as a test
 #
 macro(DEFINE_UNIT_TEST NAME LIB)
-  add_unit_test(${NAME} ${NAME}.cc)
+  ADD_UNIT_TEST(${NAME} ${NAME}.cc)
   target_link_libraries(${NAME} ${PUBLIC_KEYWORD} ${LIB})
 endmacro(DEFINE_UNIT_TEST)
